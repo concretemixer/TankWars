14b2ef41c5aab1eed015b77520a852490b64c95e
Max Pervukhin <max.pervukhin@gmail.com>
2017-12-20 18:51:56 +0700

    first

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    [SerializeField]
+    ParticleSystem explosionLand;
+
+    [SerializeField]
+    ParticleSystem explosionWater;
+    // Use this for initialization
+    float Cx;
+
+    void Start () {
+        Cx = 0.005f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void FixedUpdate()
+    {
+      
+        Vector3 v = -GetComponent<Rigidbody>().velocity;
+        float vSq = v.sqrMagnitude * Cx;
+        v.Normalize();
+        GetComponent<Rigidbody>().AddForce(v * vSq, ForceMode.Force);
+        //GetComponent<Rigidbody>().AddForce(Vector3.zero);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            explosionLand.transform.parent = transform.parent;
+            explosionLand.Play();
+            Destroy(gameObject);
+        }
+        if (other.gameObject.layer == 9)
+        {
+            explosionLand.transform.parent = transform.parent;
+            explosionLand.Play();
+            Destroy(gameObject);
+        }
+        if (other.gameObject.layer == 4)
+            Destroy(gameObject);
+    }
+}
